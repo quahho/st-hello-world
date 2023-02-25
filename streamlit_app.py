63c3ed935e641aac2ac48973e4028d2e870738f8
quahho <60967744+quahho@users.noreply.github.com>
2023-02-26 01:26:44 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,37 @@
 import streamlit as st
+import tkinter as tk
+from tkinter import filedialog
 
 st.write('Hello world!')
+
+# Set up tkinter
+root = tk.Tk()
+
+# Hide tkinter window
+root.withdraw()
+
+# Make folder picker dialog appear on top of other windows
+root.wm_attributes('-topmost', 1)
+
+# Create folder selector button
+if st.button('Choose Folder Containing CSV Subfolders'):
+
+    # Opens file explorer for selection
+    st.session_state.csv_folder_path = filedialog.askdirectory(master=root)
+
+# Check the path variable
+if len(st.session_state.csv_folder_path) > 0:
+
+    # Print folder path if selected
+    st.success(st.session_state.csv_folder_path)
+
+    # List out subfolders in the folder
+    st.text('')
+    st.markdown('**Subfolders found in the folder:**')
+
+    else:
+        st.info('No subfolders with campaign name found in this folder...')
+
+else:
+    # Informs if cancel selection
+    st.info('No folder has been selected yet...')

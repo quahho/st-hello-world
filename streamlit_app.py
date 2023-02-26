71fd87c330e70f6c86376a7b3a371447567a0847
quahho <60967744+quahho@users.noreply.github.com>
2023-02-26 17:12:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,4 @@
 import streamlit as st
 
-# Hide the default footer
-hide_footer_style = '''
-<style>
-.reportview-container .main footer {visibility: hidden;}
-'''
-
-st.markdown(hide_footer_style, unsafe_allow_html=True)
-
-# Hide the default hamburger menu
-hide_menu_style = '''
-<style>
-#MainMenu {visibility: hidden;}
-</style>
-'''
-
-st.markdown(hide_menu_style, unsafe_allow_html=True)
-
 # Test code
 st.write('Hello world!')

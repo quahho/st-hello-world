158890736993b8da108c341e82da9691c6b7e13a
quahho <60967744+quahho@users.noreply.github.com>
2023-02-27 14:49:47 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,11 +20,11 @@ authenticator = stauth.Authenticate(
 
 # creating a login widget
 name, authentication_status, username = authenticator.login('Login', 'main')
-if authentication_status:
+if st.session_state["authentication_status"]:
     authenticator.logout('Logout', 'main')
-    st.write(f'Welcome *{name}*')
+    st.write(f'Welcome *{st.session_state["name"]}*')
     st.title('Some content')
-elif authentication_status is False:
+elif st.session_state["authentication_status"] == False:
     st.error('Username/password is incorrect')
-elif authentication_status is None:
+elif st.session_state["authentication_status"] == None:
     st.warning('Please enter your username and password')

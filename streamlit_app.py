b604762a92859d2c291c3d14831cef4bf80342c3
quahho <60967744+quahho@users.noreply.github.com>
2023-02-27 10:37:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,44 +1,15 @@
-import yaml
 import streamlit as st
 import streamlit_authenticator as stauth
 
-# # Test code
-# st.write('Hello world!')
+# Test code
+st.write('Hello world!')
 
-# hashed_passwords = stauth.Hasher(['The6thSense']).generate()
-# st.write(hashed_passwords)
+import os
 
-# Import the YAML file
-with open('config.yaml') as file:
-    config = yaml.load(file, Loader=yaml.SafeLoader)
+def file_selector(folder_path='.'):
+    filenames = os.listdir(folder_path)
+    selected_filename = st.selectbox('Select a file', filenames)
+    return os.path.join(folder_path, selected_filename)
 
-# Create the authenticator object
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
-)
-
-
-# col1, col2, col3 = st.columns([1, 2, 1])
-
-# with col2:
-#    name, authentication_status, email = authenticator.login('Login', 'main')
-
-
-
-# Render the login widget by providing a name for the form and its location
-name, authentication_status, email = authenticator.login('Login', 'sidebar')
-
-
-if st.session_state["authentication_status"]:
-    authenticator.logout('Logout', 'sidebar')
-    st.title('Some content')
-
-elif st.session_state["authentication_status"] == False:
-    st.error('Username/password is incorrect')
-
-elif st.session_state["authentication_status"] == None:
-    st.sidebar.warning('Please enter your username and password')
+filename = file_selector()
+st.write('You selected `%s`' % filename)

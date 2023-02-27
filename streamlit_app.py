0eba8fcd599bea6911791df96bc20142fb847f24
quahho <60967744+quahho@users.noreply.github.com>
2023-02-27 10:57:49 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,4 @@ import streamlit_authenticator as stauth
 # Test code
 st.write('Hello world!')
 
-import os
 
-def file_selector(folder_path='.'):
-    filenames = os.listdir(folder_path)
-    selected_filename = st.selectbox('Select a file', filenames)
-    return os.path.join(folder_path, selected_filename)
-
-filename = file_selector()
-st.write('You selected `%s`' % filename)

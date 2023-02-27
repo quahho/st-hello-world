05ffbda917d12cafeaa1ccc700abc900ee2c8a9b
quahho <60967744+quahho@users.noreply.github.com>
2023-02-27 13:58:29 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,30 @@
+import yaml
 import streamlit as st
 import streamlit_authenticator as stauth
 
 # Test code
 st.write('Hello world!')
 
+# Loading config file
+with open('../config.yaml') as file:
+    config = yaml.load(file, Loader=yaml.SafeLoader)
 
+# Creating the authenticator object
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'], 
+    config['cookie']['key'], 
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+# creating a login widget
+name, authentication_status, username = authenticator.login('Login', 'main')
+if authentication_status:
+    authenticator.logout('Logout', 'main')
+    st.write(f'Welcome *{name}*')
+    st.title('Some content')
+elif authentication_status is False:
+    st.error('Username/password is incorrect')
+elif authentication_status is None:
+    st.warning('Please enter your username and password')

17752261e41e665820266485ae84a81c580bd7e2
quahho <60967744+quahho@users.noreply.github.com>
2023-03-29 08:48:46 +0800

    Update login.py

--- login.py
+++ login.py
@@ -2,7 +2,17 @@
 # Import libraries
 import bcrypt
 import streamlit as st
+from navigate import showNavigationBar
 from home import showHomePage
+from platform_csv import showPlatformCSVPage
+from platform_html import showPlatformHTMLPage
+from platform_guide import showPlatformGuidePage
+from email_eml import showEmailEMLPage
+from email_guide import showEmailGuidePage
+
+
+# ====================================================================================================
+
 
 # Set tab info
 st.set_page_config(
@@ -10,50 +20,50 @@ st.set_page_config(
     page_icon="🎏"
 )
 
+
 # ====================================================================================================
+
 
 # Initialize session variables
 
-# Variable to check user is logged in
+# Variable to check if user is logged in
 if 'login_status' not in st.session_state:
     st.session_state['login_status'] = False
 
-# Variable to check user is logged in
+# Variable to check if attempt was made
 if 'failed_attempt' not in st.session_state:
     st.session_state['failed_attempt'] = False
-
-# Variable to set input text field instruction
-if 'text_value' not in st.session_state:
-    st.session_state['text_value'] = 'Type here'
-
-# Variable to store input text submitted value
-if 'code' not in st.session_state:
-    st.session_state['code'] = ''
 
 
 # ====================================================================================================
 
-# Function to handle submitted text
-def submitText():
-
-    # Pass the submitted text to session variable
-    st.session_state['code'] = st.session_state['text_input']
-
-    # Check the submitted text for a match
-    checkCode(st.session_state['code'])
-
 
 # Function to check if code matches
-def checkCode(text_input) :
+def checkCode() :
 
     # Set real password's hash
-    hash = b'$2b$12$ZeHtfYRuDZMwjkCbJWwe7enHtBAOAUnK1O2w/ZxLEK9w6to0TEVhm'
+    hash_list = [
+        b'$2b$12$ZeHtfYRuDZMwjkCbJWwe7enHtBAOAUnK1O2w/ZxLEK9w6to0TEVhm',
+        b'$2b$12$HcmMrhPEfp1d7lBzRslPUuL3qERDaS/5TxlG/MNAhaKIKsTYQzBxS'
+    ]
 
     # Encode the input text
-    encoded_text_input = text_input.encode('utf-8')
+    encoded_text_input = st.session_state['text_input'].encode('utf-8')
 
-    # Compare encoded input text with hash
-    matched = bcrypt.checkpw(encoded_text_input, hash)
+    # Initialize match variable
+    matched = False
+
+    # Loop through the hash list
+    for hash in hash_list :
+
+        # Compare encoded input text with hash
+        matched = bcrypt.checkpw(encoded_text_input, hash)
+
+        # When found a match
+        if matched :
+
+            # Stop loop
+            break
 
     # Set path for post checking action
     if matched :
@@ -72,21 +82,9 @@ def checkCode(text_input) :
         # Trigger failed attempt 
         st.session_state['failed_attempt'] = True
 
-        
+
 # Function to display login page
 def showLoginPage() :
-
-    # Vertically centre the content of the page 
-    st.write(
-        """
-        <style>
-        [data-testid="stAppViewContainer"] {
-            padding: 10vh 1vw;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
 
     # Set the content alignment
     left_space, content, right_space = st.columns([1, 3, 1])
@@ -95,7 +93,7 @@ def showLoginPage() :
     with content :
     
         # Set logo
-        st.image('images/2x-6sense-banner.png')
+        st.image('https://raw.githubusercontent.com/quahho/st-hello-world/main/images/2x-6sense-banner.png')
 
         # Set the inner content alignment
         inner_left_space, inner_content, inner_right_space = st.columns([1, 3, 1])
@@ -103,17 +101,17 @@ def showLoginPage() :
         # Define inner content
         with inner_content:
 
-            # Create space below picture
+            # Create space
             st.write('')
 
             # Set input text box
             text_input = st.text_input(
-                label = 'Enter some text 👇',
+                label = 'Enter code',
                 key = 'text_input', 
                 type = 'password',
                 autocomplete = None,
-                on_change = submitText,
-                placeholder = st.session_state['text_value'],
+                on_change = checkCode,
+                placeholder = 'Type here',
                 label_visibility = 'collapsed'
             )
 
@@ -128,8 +126,21 @@ def showLoginPage() :
                 # Display error message
                 st.error('Invalid code, try again.')
 
+    # Vertically centre the content of the page 
+    st.write(
+        """
+        <style>
+        [data-testid="stAppViewContainer"] {
+            padding: 10vh 1vw;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
 
 # ====================================================================================================
+
 
 # Run main workflow of the page
 
@@ -141,17 +152,48 @@ if not st.session_state['login_status'] :
 
 else :
 
-    # Display home page when logged in
-    showHomePage()
+    # Display navigation bar when logged in
+    showNavigationBar()
 
+    # Display main content accordingly to checkbox values
+    if st.session_state['option_0'] :
 
+        # Display home page
+        showHomePage()
+    
+    elif st.session_state['option_1'] :
+
+        # Display platform csv page
+        showPlatformCSVPage()
+
+    elif st.session_state['option_2'] :
+
+        # Display platform html page
+        showPlatformHTMLPage()
+    
+    elif st.session_state['option_3'] :
+
+        # Display platform guide page
+        showPlatformGuidePage()
+
+    elif st.session_state['option_4'] :
+
+        # Display email eml page
+        showEmailEMLPage()
+
+    elif st.session_state['option_5'] :
+
+        # Display email guide page
+        showEmailGuidePage()
+    
+    
 # Hide the hamburger icon and "Made with Streamlit" footer
 st.write(
     """
     <style>
-    #MainMenu {
-        visibility: hidden;
-    }
+    # #MainMenu {
+    #     visibility: hidden;
+    # }
     footer {
         visibility: hidden;
     }
